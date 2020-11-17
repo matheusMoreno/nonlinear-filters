a814be8d5fbc9120ba1e7c586f9e310c43a30af6
Matheus Moreno <matheus.moreno.08@gmail.com>
2020-11-17 20:33:05 -0300

    Remove Lordelo's directory and other gitignore files

--- AutomaticMusicTrackRemoval/Compression/code/mycon.m
+++
@@ -1,8 +0,0 @@
-% Date: 15/11/2015
-% Author: Leonardo T. Duarte (FCA/UNICAMP)
-
-function [c,ceq] = mycon(w,Xpoly)
-
-c = [];
-ceq  = w'*(Xpoly)*Xpoly'*w - 1;
-
--- AutomaticMusicTrackRemoval/Compression/code/normSL0_NL.m
+++
@@ -1,7 +0,0 @@
-% Date: 15/11/2015
-% Author: Leonardo T. Duarte (FCA/UNICAMP)
-
-function J = normSL0_NL(w,Xpoly,phi)
-
-J = SL0_norm(dct(Xpoly'*w),phi);
-
