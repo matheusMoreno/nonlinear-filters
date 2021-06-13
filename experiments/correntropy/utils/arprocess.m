97a14f9d2ebfc11bb54d4ba0d609f3d2f9a0288b
Matheus Moreno <matheus.moreno@poli.ufrj.br>
2021-06-13 00:48:13 -0300

    Add function that gens signal similar to soundwave

---
+++ experiments/correntropy/utils/arprocess.m
@@ -0,0 +1,26 @@
+function signal = arprocess(N, Ts, duration)
+    %ARPROCESS generates a signal that behaves like a soundwave.
+
+    assert(mod(N, 2) == 0, 'N has to be even.');
+
+    N = N / 2;
+
+    % Generate poles
+    angles = pi * rand(N, 1);
+    radius = 0.75 + (0.99 - 0.75) * rand(N, 1);
+    poles = [radius .* exp(1i * angles); radius .* exp(-1i * angles)];
+
+    % Create system
+    sys = zpk([], poles, 1, Ts);
+
+    % Generate desired signal (no normalization)
+    t = 0:Ts:duration;
+    u = wgn(length(t), 1, 0);
+    signal = lsim(sys, u, t);
+
+    % Normalize signal so that |s| <= 0.5
+    maxVal = max(abs(signal));
+    g = 0.5 / maxVal;
+    signal = g .* signal;
+    signal = signal(:);
+end
