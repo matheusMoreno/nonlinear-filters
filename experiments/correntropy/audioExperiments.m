c07c373be7d47f20285b0f4afa6d5cbd25db8db5
Matheus Moreno <matheus.moreno@poli.ufrj.br>
2021-06-16 16:27:51 -0300

    Final version of audio/PoC correntropy experiments

--- experiments/correntropy/audioExperiments.m
+++ experiments/correntropy/audioExperiments.m
@@ -1,6 +1,6 @@
-% Experiments for the nonlinear discrete Kernel Wiener filter
+% Experiments for the nonlinear discrete Correntropy filter
 % By Matheus F. Moreno - SMT/DEL/UFRJ
-% March 2021
+% Jun 2021
 %
 % NOTES:
 %   - By convention, x_1 is always the reference signal
@@ -13,16 +13,17 @@ addpath('../../correntropy')
 
 %% Create results file
 
-fprintf('KERNEL WIENER FILTER RESULTS\n');
-fd = fopen('results.txt', 'w');
-fprintf(fd, 'KERNEL WIENER FILTER RESULTS\n');
+results_filepath = 'results_audio.txt';
+fprintf('CORRENTROPY FILTER RESULTS\n');
+fd = fopen(results_filepath, 'w');
+fprintf(fd, 'CORRENTROPY FILTER RESULTS\n');
 fclose(fd);
 
 
 %% Import audios and plot spectograms
 
 % Reference signal
-x_filepath = '.\x\x_1.wav';
+x_filepath = './x/x_1.wav';
 [x, Fs_x] = audioread(x_filepath);
 
 % Assert that the audio is mono
@@ -39,13 +40,13 @@ ylim(gca, [0, 10]);
 colormap('bone');
 colorbar('off');
 xlabel('Tempo [s]');
-ylabel('Frequ�ncia [kHz]');
+ylabel('Frequência [kHz]');
 set(gcf,'Position',[10 10 1250 400]);
 saveas(gcf, 'wiener-reference-signal.eps', 'epsc');
 
 
 % Dialogue signal
-x2_filepath = '.\x\x_2.wav';
+x2_filepath = './x/x_2.wav';
 [x2, Fs_x2] = audioread(x2_filepath);
 
 assert(size(x2, 2) == 1, 'The audio must be mono.')
@@ -61,7 +62,7 @@ ylim(gca, [0, 20]);
 colormap('bone');
 colorbar('off');
 xlabel('Tempo [s]');
-ylabel('Frequ�ncia [kHz]');
+ylabel('Frequência [kHz]');
 set(gcf,'Position',[10 10 1250 400]);
 saveas(gcf, 'wiener-dialogue-signal.eps', 'epsc');
 
@@ -69,101 +70,95 @@ saveas(gcf, 'wiener-dialogue-signal.eps', 'epsc');
 %% Do the experiments
 
 % Filter parameters
-L = 34;
-M = 4096 * 2;
-sigmas = [1e-2; 3e-2; 1e-1; 3e-1; 1; 3; 10];
+M = 8820;
+L = 1;
+sigma = 0;
 
-x_gpu = gpuArray(x);
+parameters_str = strjoin({
+    '\n\n###############################\n\n',                  ...
+    sprintf('\nFilter length: L = %d\n', L),                    ...
+    sprintf('Window size: W = %d\n', M),                        ...
+    sprintf('Hop: H = %d (%.2f overlap)\n', round(M/2), 0.5)    ...
+}, '');
+fprintf(parameters_str);
+fd = fopen(results_filepath, 'a');
+fprintf(fd, parameters_str);
+fclose(fd);
 
-for sigma = sigmas
-    parameters_str = strjoin({
-        '\n###############################\n\n', ...
-        sprintf('\nFilter length: L = %d\n', L), ...
-        sprintf('Window size: W = %d\n', M), ...
-        sprintf('Hop: H = %d (%.2f overlap)\n', round(M/2), 0.5), ...
-        sprintf('Sigma: s = %.2f\n', sigma), ...
+for i = 4:4
+    % Import observed signal
+    y_filepath = strjoin({'./y/y_', num2str(i), '.wav'}, '');
+    [y, Fs_y] = audioread(y_filepath);
+
+    % Remove paddings, if present
+    len_pad = length(y) - length(x);
+    if len_pad > 0
+        start_pad = 528;
+        end_pad = len_pad - start_pad;
+        y = y(start_pad + 1:end - end_pad);
+    end
+
+    % Assertions
+    assert(size(y, 2) == 1, 'The audio must be mono.');
+    assert(Fs_x == Fs_y, 'The sampling rate of x and d does not match.');
+    assert(length(y) == length(x), 'x and d does not have the same length.');
+
+
+    % Import filtered reference signal
+    d_filepath = strjoin({'./d/d_', num2str(i), '.wav'}, '');
+    [d, Fs_d] = audioread(d_filepath);
+
+    % Remove paddings, if present
+    len_pad = length(d) - length(x);
+    if len_pad > 0
+        start_pad = 528;
+        end_pad = len_pad - start_pad;
+        d = d(start_pad + 1:end - end_pad);
+    end
+
+    % Assertions
+    assert(size(d, 2) == 1, 'The audio must be mono.');
+    assert(Fs_x == Fs_d, 'The sampling rate of x and y does not match.');
+    assert(length(d) == length(x), 'x and y does not have the same length.');
+
+
+    % Estimate the signal
+    d_hat = correntropyFilterCola(x, y, L, sigma, M);
+
+
+    % Compute metrics
+    SDR_xy = sdr(d, x);
+    PAQM_score_xy = PAQM(d, x, Fs_d);
+    [Rnonlin_score_xy, ~, ~, ~] = Rnonlintest([d, d], [x, x], Fs_d);
+    Rnonlin_score_xy = log10(Rnonlin_score_xy);
+
+    SDR_yyhat = sdr(d, d_hat);
+    PAQM_score_yyhat = PAQM(d, d_hat, Fs_d);
+    [Rnonlin_score_yyhat, ~, ~, ~] = Rnonlintest([d, d], [d_hat, d_hat], Fs_d);
+    Rnonlin_score_yyhat = log10(Rnonlin_score_yyhat);
+
+
+    % Print and append results to file
+    results_str = strjoin({
+        '\n-------------------------\n\n', ...
+        sprintf('EXPERIMENT %d\n\n', i), ...
+        sprintf('Reference signal (x) metrics:\n'), ...
+        sprintf('SDR = %3.2f dB\n', SDR_xy), ...
+        sprintf('PAQM score = %3.4f\n', PAQM_score_xy), ...
+        sprintf('Rnonlin score (log) = %.4f\n\n', Rnonlin_score_xy), ...
+        sprintf('Estimated signal (d_hat) metrics:\n'), ...
+        sprintf('SDR = %3.2f dB\n', SDR_yyhat), ...
+        sprintf('PAQM score = %3.4f\n', PAQM_score_yyhat), ...
+        sprintf('Rnonlin score (log) = %.4f\n', Rnonlin_score_yyhat)
     }, '');
-    fprintf(parameters_str);
-    fd = fopen('results.txt', 'a');
-    fprintf(fd, parameters_str);
+
+    fprintf(results_str);
+    fd = fopen(results_filepath, 'a');
+    fprintf(fd, results_str);
     fclose(fd);
 
-    for i = 1:6
-        % Import observed signal
-        y_filepath = strjoin({'.\y\y_', num2str(i), '.wav'}, '');
-        [y, Fs_y] = audioread(y_filepath);
 
-        % Remove paddings, if present
-        len_pad = length(y) - length(x);
-        if len_pad > 0
-            start_pad = 528;
-            end_pad = len_pad - start_pad;
-            y = y(start_pad + 1:end - end_pad);
-        end
-
-        % Assertions
-        assert(size(y, 2) == 1, 'The audio must be mono.');
-        assert(Fs_x == Fs_y, 'The sampling rate of x and d does not match.');
-        assert(length(y) == length(x), 'x and d does not have the same length.');
-
-
-        % Import filtered reference signal
-        d_filepath = strjoin({'.\d\d_', num2str(i), '.wav'}, '');
-        [d, Fs_d] = audioread(d_filepath);
-
-        % Remove paddings, if present
-        len_pad = length(d) - length(x);
-        if len_pad > 0
-            start_pad = 528;
-            end_pad = len_pad - start_pad;
-            d = d(start_pad + 1:end - end_pad);
-        end
-
-        % Assertions
-        assert(size(d, 2) == 1, 'The audio must be mono.');
-        assert(Fs_x == Fs_d, 'The sampling rate of x and y does not match.');
-        assert(length(d) == length(x), 'x and y does not have the same length.');
-
-
-        % Estimate the signal
-        y_gpu = gpuArray(y);
-        d_hat = kernelWienerCola(x_gpu, y_gpu, L, M, sigma);
-
-
-        % Compute metrics
-        SDR_xy = sdr(d, x);
-        PAQM_score_xy = PAQM(d, x, Fs_d);
-        [Rnonlin_score_xy, ~, ~, ~] = Rnonlintest([d, d], [x, x], Fs_d);
-        Rnonlin_score_xy = log10(Rnonlin_score_xy);
-
-        SDR_yyhat = sdr(d, d_hat);
-        PAQM_score_yyhat = PAQM(d, d_hat, Fs_d);
-        [Rnonlin_score_yyhat, ~, ~, ~] = Rnonlintest([d, d], [d_hat, d_hat], Fs_d);
-        Rnonlin_score_yyhat = log10(Rnonlin_score_yyhat);
-
-
-        % Print and append results to file
-        results_str = strjoin({
-            '\n-------------------------\n\n', ...
-            sprintf('EXPERIMENT %d\n\n', i), ...
-            sprintf('Reference signal (x) metrics:\n'), ...
-            sprintf('SDR = %3.2f dB\n', SDR_xy), ...
-            sprintf('PAQM score = %3.4f\n', PAQM_score_xy), ...
-            sprintf('Rnonlin score (log) = %.4f\n\n', Rnonlin_score_xy), ...
-            sprintf('Estimated signal (d_hat) metrics:\n'), ...
-            sprintf('SDR = %3.2f dB\n', SDR_yyhat), ...
-            sprintf('PAQM score = %3.4f\n', PAQM_score_yyhat), ...
-            sprintf('Rnonlin score (log) = %.4f\n', Rnonlin_score_yyhat)
-        }, '');
-
-        fprintf(results_str);
-        fd = fopen('results.txt', 'a');
-        fprintf(fd, results_str);
-        fclose(fd);
-
-
-        % Save audio estimation
-        d_hat_filepath = strjoin({'.\d_hat\d_hat_', num2str(i), '.wav'}, '');
-        audiowrite(d_hat_filepath, d_hat, Fs_d);
-    end
+    % Save audio estimation
+    d_hat_filepath = strjoin({'./d_hat/d_hat_', num2str(i), '.wav'}, '');
+    audiowrite(d_hat_filepath, d_hat, Fs_d);
 end
--- experiments/correntropy/pocExperiments.m
+++ experiments/correntropy/pocExperiments.m
@@ -1,7 +1,169 @@
-% Proof-of-concept experiments for the Correntropy Filter
+% Proof of concept experiments for the Correntropy Filter
 % By Matheus F. Moreno - SMT/DEL/UFRJ
-% March 2021
+% July 2021
 
-%% Add path for current method
 
-addpath('../../correntropy')
+%% Add paths for methods and utilitary functions
+
+addpath('../../correntropy:../../wiener')
+addpath('../../sdr')
+addpath('utils')
+
+
+%% Create results file
+
+header = 'CORRENTROPY FILTER RESULTS (PROOF OF CONCEPT EXPERIMENTS)\n';
+fprintf(header);
+
+signals_dir = 'signals_poc';
+mkdir(signals_dir);
+results_filename = 'results_poc.txt';
+fd = fopen(results_filename, 'w');
+fprintf(fd, header);
+fclose(fd);
+
+
+%% Define some functions that will be used
+
+% Lowpass filter (moving average)
+lowpass = @(x) fftfilt(ones(150, 1) / 150, x);
+
+% Soft clipping function
+softclip = @(x) 2 .* atan(1.5 .* x) ./ pi;
+
+% Compute power of signal
+pwr = @(x) mean(x .^ 2);
+
+% Normalized mean square error
+nmse = @(x, xhat) mean((x - xhat) .^ 2) / pwr(x);
+
+
+%% Generate signals for testing
+
+fs_s = 2000;
+Ts_s = 1 / fs_s;
+duration = 1;
+t_sine = 0:Ts_s:duration;
+t_sine = t_sine(:);
+
+% Sinewave with harmonicse
+basef = 3;
+As = [0.4 -0.2 -0.1 -0.2 0.4];
+sine = zeros(length(t_sine), 1);
+for k = 1:length(As)
+    sine = sine + As(k) .* sin(2 * pi * k * basef * t_sine);
+end
+sine = sine(:);
+
+% White noise filtered through an AR process
+fs_ar = 44100;
+Ts_ar = 1 / fs_ar;
+fauxaudio = arprocess(80, Ts_ar, 0.2);
+fauxaudio = fauxaudio(:);
+
+% White noise for experiment 5
+noise = wgn(length(t_sine), 1, -20);
+noise = noise(:);
+
+% Another faux audio signal for experiment 6
+fauxdeux = arprocess(80, Ts_ar, 0.2);
+fauxdeux = fauxdeux(:);
+
+% Plot signals
+plot(t_sine, sine)
+saveas(gcf, 'sine.png')
+
+std_str = strjoin({                                                    ...
+    '\nSTANDARD DEVIATION OF SIGNALS\n',                               ...
+    sprintf('Sinewave: %.5f\n', std(sine)),                            ...
+    sprintf('Faux audiowave A: %.5f\n', std(fauxaudio))                ...
+}, '');
+fprintf(std_str);
+fd = fopen(results_filename, 'a');
+fprintf(fd, std_str);
+fclose(fd);
+
+dlmwrite(                                                                  ...
+    strjoin({signals_dir, '/sine.csv'}, ''), [t_sine sine],                ...
+    'delimiter', ' ', 'precision', 10                                      ...
+);
+pause
+
+
+%% Define experiments' vectors and parameters
+
+
+originals = {sine fauxaudio sine fauxaudio sine fauxaudio};
+
+objectives = {                                                             ...
+    lowpass(sine) lowpass(fauxaudio) softclip(sine) softclip(fauxaudio)    ...
+    softclip(sine) softclip(fauxaudio)                                     ...
+};
+
+mixings = {objectives{1:4} (softclip(sine) + noise) 0.5 * (softclip(fauxaudio) + fauxdeux)};
+
+wienerLs = [10 10 34 34 34 34];
+correnLs = [10 10 1 1 1 1];
+sigmas = [1e-10 1e-10 0.9 0.9 0.9 0.9];
+
+
+%% Execute experiments
+
+for k = 1:6
+    original = originals{k};
+    objective = objectives{k};
+    mixing = mixings{k};
+
+    Lw = wienerLs(k);
+    Lc = correnLs(k);
+    sig = sigmas(k);
+
+    wiener_est = wiener(original, mixing, Lw);
+    if sig ~= 0
+        corren_est = correntropyFilter(original, mixing, Lc, sig);
+    else % if sig == 0, different values are tested
+        min_nmse = 1;
+        subsigs = logspace(-5, 2, 200);
+        for s = subsigs
+            corren_est_s = correntropyFilter(original, mixing, Lc, s);
+            nmse_s = nmse(objective, corren_est_s);
+            if nmse_s < min_nmse
+                sig = s;
+                corren_est = corren_est_s;
+                min_nmse = nmse_s;
+            end
+        end
+    end
+
+    experiment_str = strjoin({                                                ...
+        '\n-------------------------\n\n',                                    ...
+        sprintf('EXPERIMENT %d\n', k),                                        ...
+        sprintf('Lw = %d; Lc = %d, sig = %.10f\n', Lw, Lc, sig),              ...
+        sprintf('Wiener NMSE: %.8f\n', nmse(objective, wiener_est)),          ...
+        sprintf('Wiener SDR: %.2f dB\n', sdr(objective, wiener_est)),         ...
+        sprintf('Correntropy NMSE: %.8f\n', nmse(objective, corren_est)),     ...
+        sprintf('Correntropy SDR: %.2f dB\n', sdr(objective, corren_est))     ...
+    }, '');
+
+    % Save results to file
+    fprintf(experiment_str);
+    fd = fopen(results_filename, 'a');
+    fprintf(fd, experiment_str);
+    fclose(fd);
+
+    % Save signals for experiments with sinewaves
+    if mod(k, 2) == 1
+        plot(t_sine, objective)
+        hold on
+        plot(t_sine, wiener_est)
+        plot(t_sine, corren_est)
+        saveas(gcf, 'sine.png')
+        hold off
+
+        dlmwrite(                                                          ...
+            sprintf('%s/experiment_%d.csv', signals_dir, k),               ...
+            [t_sine objective wiener_est corren_est],                      ...
+            'delimiter', ' ', 'precision', 10                              ...
+        )
+    end
+end
