ff8e553bda12c569126da4f538b83caa4ef95fb2
Matheus Moreno <matheus.moreno@poli.ufrj.br>
2021-06-16 16:26:27 -0300

    Optimize correntropy funcs for GPUs

--- correntropy/correntropyFilterCola.m
+++ correntropy/correntropyFilterCola.m
@@ -1,10 +1,15 @@
-function d_est = correntropyFilterCola(x, d, L, M, sigma)
+function d_est = correntropyFilterCola(x, d, L, sigma, M)
 %CORRENTROPYFILTERCOLA estimates nonlinear filtering with constant overlap-add method
 %   x: input signal to be filtered
 %   d: output signal, correlated with x
 %   L: size of each filter
-%   M: window size
 %   sigma: parameter of the kernel function
+%   M: window size
+%
+%   If sigma = 0, the filter chooses the best sigma for each block.
+%   This function is optimized for machines with GPUs.
+
+    addpath('../../sdr');
 
     assert(length(x) == length(d), 'x and d must have the same length.');
     assert(mod(M, 2) == 0, 'The window size must be even.')
@@ -12,25 +17,22 @@ function d_est = correntropyFilterCola(x, d, L, M, sigma)
     % Set constants
     N = length(d);
 
-    % Make sure x and d are column vectors
-    x = x(:);
-    d = d(:);
+    % Make sure x and d are GPU arrays
+    x = gpuArray(x(:));
+    d = gpuArray(d(:));
 
     % Initialize estimate and index
-    %win = hann(M, 'periodic');
-    %overlap = round(M / 2);
-    win = ones(M, 1);
-    overlap = 0;
+    win = gpuArray(hann(M, 'periodic'));
+    overlap = round(M / 2);
 
     % Instantiating the estimated signal
-    d_est = zeros(N + M - 1, 1);
+    d_est = gpuArray(zeros(N + M - 1, 1));
 
     i = 1;
     while i <= N
-        fprintf('At index %d\n', i);
         % Instantiate x_i and d_i
-        x_i = zeros(M, 1);
-        d_i = zeros(M, 1);
+        x_i = gpuArray(zeros(M, 1));
+        d_i = gpuArray(zeros(M, 1));
 
         % Getting the x(n) and d(n) for this iteration
         ind = min(i + M - 1, N);
@@ -39,21 +41,62 @@ function d_est = correntropyFilterCola(x, d, L, M, sigma)
 
         % Create initial conditions
         init_cond_x = x(max(i - L + 1, 1):i - 1);
-        init_cond = [zeros(L - 1 - length(init_cond_x), 1); init_cond_x];
+        init_cond = [gpuArray(zeros(L - 1 - length(init_cond_x), 1)); init_cond_x];
 
-        % Calculate the sum(sum()) with a 2D convolution
-        corr = xcorr2(                                              ...
-            gaussianKernel(                                         ...
-                repmat(flip([init_cond; x_i]), 1, M + L - 1),       ...
-                repmat(flip([init_cond; x_i]), 1, M + L - 1)',      ...
-                sigma, 'elemwise'                                   ...
-            ),                                                      ...
-            pinv(correntropyMatrix(x_i, L, sigma))                        ...
-        );
-        corr = corr(L:L + M - 1, L:L + M - 1);
+        if sigma == 0
+            sdr_i = -inf;
+            for s = logspace(-9, 1, 250)
+                % Calculate the sum(sum()) with a 2D convolution
+                corr = xcorr2(                                              ...
+                    gaussianKernel(                                         ...
+                        repmat(flip([init_cond; x_i]), 1, M + L - 1),       ...
+                        repmat(flip([init_cond; x_i]), 1, M + L - 1)',      ...
+                        s, 'elemwise'                                       ...
+                    ),                                                      ...
+                    pinv(correntropyMatrix(x_i, L, s, 'gpu'))               ...
+                );
+                corr = corr(L:L + M - 1, L:L + M - 1);
 
-        % Calculate the last sum with a dot product
-        d_est_i = flip(corr * flip(d_i)) / M;
+                % Calculate the last sum with a dot product
+                d_est_ijk = flip(corr * flip(d_i)) / M;
+
+                % Gain correction
+                scale = std(d_est_ijk) / std(d_i);
+                if scale ~= 0 && ~isnan(scale)
+                    d_est_ijk = d_est_ijk / scale;
+                end
+
+                % Check if SDR got bigger
+                sdr_ijk = sdr(d_i, d_est_ijk);
+                if sdr_ijk > sdr_i
+                    sdr_i = sdr_ijk;
+                    d_est_i = d_est_ijk;
+                    s_best = s;
+                end
+            end
+        else
+            % Calculate the sum(sum()) with a 2D convolution
+            corr = xcorr2(                                              ...
+                gaussianKernel(                                         ...
+                    repmat(flip([init_cond; x_i]), 1, M + L - 1),       ...
+                    repmat(flip([init_cond; x_i]), 1, M + L - 1)',      ...
+                    sigma, 'elemwise'                                   ...
+                ),                                                      ...
+                pinv(correntropyMatrix(x_i, L, sigma, 'gpu'))           ...
+            );
+            corr = corr(L:L + M - 1, L:L + M - 1);
+
+            % Calculate the last sum with a dot product
+            d_est_i = flip(corr * flip(d_i)) / M;
+
+            % Gain correction
+            scale = std(d_est_i) / std(d_i);
+            if scale ~= 0 && ~isnan(scale)
+                d_est_i = d_est_i / scale;
+            end
+        end
+
+        fprintf('Index %d, L = %d, s = %.10f, SDR = %.2f dB\n', i, L, s_best, sdr_i);
 
         % Adding the values with a window
         d_est(i:i + M - 1) = d_est(i:i + M - 1) + win .* d_est_i;
@@ -63,10 +106,5 @@ function d_est = correntropyFilterCola(x, d, L, M, sigma)
 
     % Setting the final variables
     d_est = d_est(1:N);
-
-    % Gain correction
-    scale = std(d_est) / std(d);
-    if scale ~= 0 && ~isnan(scale)
-        d_est = d_est / scale;
-    end
+    d_est = gather(d_est);
 end
--- correntropy/correntropyMatrix.m
+++ correntropy/correntropyMatrix.m
@@ -1,6 +1,10 @@
-function V = correntropyMatrix(x, L, sigma)
+function V = correntropyMatrix(x, L, sigma, putype)
 %CORRENTROPYMATRIX returns correntropy matrix V of size LxL for a SSS process x
 %   Computes the correntropy for x as seen on Pokharel et al. (2006)
+
+    if (~exist('putype', 'var'))
+        putype = 'cpu';
+    end
 
     % Make x a column vector
     x = x(:);
@@ -8,10 +12,14 @@ function V = correntropyMatrix(x, L, sigma)
     % Check if V can be computed
     assert(L <= length(x), 'V dims must not exceed signal length.');
 
-    % Auxiliary function to calculate v without loops
-    v_fun = @(m) mean(gaussianKernel(x(m + 1:end), x(1:end - m), ...
-        sigma, 'elemwise'));
+    v = zeros(L, 1);
+    if strcmp(putype, 'gpu')
+        v = gpuArray(v);
+    end
 
     % Generating the matrix
-    V = toeplitz(arrayfun(v_fun, 0:L - 1));
+    for m = 0:L - 1
+        v(m + 1) = mean(gaussianKernel(x(m + 1:end), x(1:end - m), sigma, 'elemwise'));
+    end
+    V = toeplitz(v);
 end
